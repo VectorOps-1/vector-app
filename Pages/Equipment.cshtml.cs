38bb40b67c44a6e082c183373cd8f05f3a935070
VectorOps-1 <admin@vectoropsgroup.com>
2026-04-18 04:08:50 +0300

    Create Equipment.cshtml.cs

---
+++ Pages/Equipment.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace vector_app_local.Pages
+{
+    public class EquipmentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
