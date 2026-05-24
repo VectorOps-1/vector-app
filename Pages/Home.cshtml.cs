d925daf1b8bc1012c2467b72256c1bc2bcefee74
VectorOps-1 <admin@vectoropsgroup.com>
2026-05-24 12:45:33 +0300

    Track selected access view on home page

--- Pages/Home.cshtml.cs
+++ Pages/Home.cshtml.cs
@@ -4,8 +4,11 @@ namespace vector_app_local.Pages;
 
 public class HomeModel : PageModel
 {
-    public void OnGet()
+    public string AccessView { get; private set; } = "manager";
+
+    public void OnGet(string? access)
     {
         ViewData["ClientName"] = "Client Business Name";
+        AccessView = access == "operational-staff" ? "operational-staff" : "manager";
     }
 }
