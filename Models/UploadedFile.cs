4ee072f972afff7dd8cd4b3103eea90fa19a222e
VectorOps-1 <admin@vectoropsgroup.com>
2026-05-24 13:26:52 +0300

    Add uploaded file model

---
+++ Models/UploadedFile.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace vector_app_local.Models;
+
+public class UploadedFile
+{
+    public int Id { get; set; }
+
+    public int ChecklistTemplateId { get; set; }
+    public ChecklistTemplate? ChecklistTemplate { get; set; }
+
+    [MaxLength(260)]
+    public string OriginalFileName { get; set; } = string.Empty;
+
+    [MaxLength(120)]
+    public string ContentType { get; set; } = string.Empty;
+
+    [MaxLength(520)]
+    public string StoragePath { get; set; } = string.Empty;
+
+    public long SizeBytes { get; set; }
+    public DateTime UploadedAtUtc { get; set; } = DateTime.UtcNow;
+}
