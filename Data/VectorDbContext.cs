9d1890995ad065415275ff2017bdee99986900f9
VectorOps-1 <admin@vectoropsgroup.com>
2026-05-26 13:03:04 +0300

    Register core operational database tables

--- Data/VectorDbContext.cs
+++ Data/VectorDbContext.cs
@@ -9,8 +9,82 @@ public class VectorDbContext : DbContext
     {
     }
 
+    public DbSet<Company> Companies => Set<Company>();
+    public DbSet<AppUser> AppUsers => Set<AppUser>();
+    public DbSet<AppRole> AppRoles => Set<AppRole>();
+    public DbSet<TaskItem> TaskItems => Set<TaskItem>();
+    public DbSet<TaskEvent> TaskEvents => Set<TaskEvent>();
+    public DbSet<AuditLog> AuditLogs => Set<AuditLog>();
+
     public DbSet<ChecklistTemplate> ChecklistTemplates => Set<ChecklistTemplate>();
     public DbSet<ChecklistSection> ChecklistSections => Set<ChecklistSection>();
     public DbSet<ChecklistItem> ChecklistItems => Set<ChecklistItem>();
     public DbSet<UploadedFile> UploadedFiles => Set<UploadedFile>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<AppRole>()
+            .HasIndex(role => role.Name)
+            .IsUnique();
+
+        modelBuilder.Entity<AppUser>()
+            .HasIndex(user => new { user.CompanyId, user.Email })
+            .IsUnique();
+
+        modelBuilder.Entity<AppUser>()
+            .HasOne(user => user.Company)
+            .WithMany(company => company.Users)
+            .HasForeignKey(user => user.CompanyId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<AppUser>()
+            .HasOne(user => user.AppRole)
+            .WithMany(role => role.Users)
+            .HasForeignKey(user => user.AppRoleId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<TaskItem>()
+            .HasOne(task => task.Company)
+            .WithMany(company => company.Tasks)
+            .HasForeignKey(task => task.CompanyId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<TaskItem>()
+            .HasOne(task => task.AssignedToUser)
+            .WithMany(user => user.AssignedTasks)
+            .HasForeignKey(task => task.AssignedToUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<TaskItem>()
+            .HasOne(task => task.AssignedByUser)
+            .WithMany(user => user.CreatedTasks)
+            .HasForeignKey(task => task.AssignedByUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<TaskEvent>()
+            .HasOne(taskEvent => taskEvent.TaskItem)
+            .WithMany(task => task.Events)
+            .HasForeignKey(taskEvent => taskEvent.TaskItemId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<TaskEvent>()
+            .HasOne(taskEvent => taskEvent.PerformedByUser)
+            .WithMany(user => user.TaskEvents)
+            .HasForeignKey(taskEvent => taskEvent.PerformedByUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<AuditLog>()
+            .HasOne(auditLog => auditLog.Company)
+            .WithMany(company => company.AuditLogs)
+            .HasForeignKey(auditLog => auditLog.CompanyId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<AuditLog>()
+            .HasOne(auditLog => auditLog.AppUser)
+            .WithMany(user => user.AuditLogs)
+            .HasForeignKey(auditLog => auditLog.AppUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
