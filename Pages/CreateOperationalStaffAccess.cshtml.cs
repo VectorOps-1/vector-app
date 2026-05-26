92343f56b3e52d6a21282ad905e5958b77d91d2e
VectorOps-1 <admin@vectoropsgroup.com>
2026-05-26 07:11:57 +0300

    Add operational staff access page model

---
+++ Pages/CreateOperationalStaffAccess.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace vector_app_local.Pages;
+
+public class CreateOperationalStaffAccessModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
